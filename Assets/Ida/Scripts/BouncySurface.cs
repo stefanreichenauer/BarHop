fce0ab02fe62be2bfefb41c3d9cb585305738dba
imaspohi <ida.stetter@gmail.com>
2025-01-24 22:31:27 +0100

    Different approach to bounciness (that actually works)

--- Assets/Ida/Scripts/BouncySurface.cs
+++ Assets/Ida/Scripts/BouncySurface.cs
@@ -5,30 +5,19 @@ using UnityEngine;
 public class BouncySurface : MonoBehaviour
 {
     [SerializeField] private float bounciness;
-
-    private void Start()
+    private Rigidbody otherRigidbody;
+    private void OnCollisionExit(Collision collision)
     {
-        Vector3 vector3 = new Vector3(0, 1, 0);
-        Vector3 vector31 = new Vector3(1, 0, 0);
-        Debug.Log($"{Vector3.Reflect(vector3, vector31)}");
+        if (otherRigidbody != null)
+        {
+            otherRigidbody.linearVelocity *= bounciness;
+
+        }
     }
     void OnCollisionEnter(Collision other)
     {
-        Vector3 normalVector = other.GetContact(0).normal;
+        otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
         
-        Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
-        if (otherRigidbody != null)
-        {
-            Vector3 directionVector = Vector3.Reflect(otherRigidbody.linearVelocity, normalVector);
-            if (directionVector == otherRigidbody.linearVelocity)
-            {
-                otherRigidbody.linearVelocity = directionVector * bounciness * (-1);
-            }
-            else
-            {
-                otherRigidbody.linearVelocity = directionVector*bounciness;
-            }
-        }
 
     }
 }
