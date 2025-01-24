c1454b7b555c6d98cb8eb0842bd0488e65cd2ce0
imaspohi <ida.stetter@gmail.com>
2025-01-24 21:51:38 +0100

    Fix collider collision problem

--- Assets/Ida/Scripts/BouncySurface.cs
+++ Assets/Ida/Scripts/BouncySurface.cs
@@ -1,21 +1,34 @@
 using NUnit.Framework.Constraints;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(BoxCollider))]
 public class BouncySurface : MonoBehaviour
 {
     [SerializeField] private float bounciness;
-    private Collision collision;
 
     private void Start()
     {
-        collision = GetComponent<Collision>();
+        Vector3 vector3 = new Vector3(0, 1, 0);
+        Vector3 vector31 = new Vector3(1, 0, 0);
+        Debug.Log($"{Vector3.Reflect(vector3, vector31)}");
     }
-    void OnCollisionEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        Vector3 myNormalVector = collision.GetContact(0).normal;
+        Vector3 normalVector = other.GetContact(0).normal;
+        
         Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
-        otherRigidbody.linearVelocity = Vector3.Reflect(otherRigidbody.linearVelocity, myNormalVector);
+        if (otherRigidbody != null)
+        {
+            Vector3 directionVector = Vector3.Reflect(otherRigidbody.linearVelocity, normalVector);
+            if (directionVector == otherRigidbody.linearVelocity)
+            {
+                otherRigidbody.linearVelocity = directionVector * bounciness * (-1);
+            }
+            else
+            {
+                otherRigidbody.linearVelocity = directionVector*bounciness;
+            }
+        }
 
     }
 }
