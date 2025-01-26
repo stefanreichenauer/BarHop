709db27906f7782795a35c81fec04af3ddce2d5a
Irena Ruprecht <irena.ruprecht@gmail.com>
2025-01-26 11:49:53 +0100

    splash effect

---
+++ Assets/Irena/Scripts/SplashAnimator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class SplashAnimator : MonoBehaviour
+{
+    public GameObject effectParent = null;
+    private ParticleSystem[] particleSystems = null;
+
+    private void Awake()
+    {
+        particleSystems = effectParent.GetComponentsInChildren<ParticleSystem>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(ParticleSystem p in particleSystems)
+        {
+            if (!p.isPlaying)
+            {
+                Debug.Log("Trigger enter");
+                p.Play();
+            }
+        }
+    }
+}
